e244b5569f6307f1cdd27b8f2e88f6d9aa4a6563
unknown <dawidgorski0000@gmail.com>
2023-04-13 00:45:45 +0200

    feat: add privacy policy and terms of service

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,8 @@
+export function Footer() {
+    return (
+        <footer className='footer_container'>
+            <a className='footer_link' href='/api/files/terms-of-service' target="_blank">Terms Of Service</a>
+            <a className='footer_link' href='/api/files/privacy-policy' target="_blank">Privacy Policy</a>
+        </footer>
+    )
+}
