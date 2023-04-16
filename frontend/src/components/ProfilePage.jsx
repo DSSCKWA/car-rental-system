01c5f695e97676aad2218009cd796606d57f9e82
unknown <dawidgorski0000@gmail.com>
2023-04-17 00:46:48 +0200

    feat: add change password feature

---
+++ frontend/src/components/ProfilePage.jsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom'
+
+export function ProfilePage(props) {
+    const { user } = props
+
+    return (
+        <div className='profile_page'>
+            <div className='profile'>
+                <h1>User Profile</h1>
+                <p>Email Address: {user?.user_email_address}</p>
+                <p>Name: {user?.name}</p>
+                <p>Surname: {user?.surname}</p>
+                <p>Permissions: {user?.permissions}</p>
+                <p>Phone Number: {user?.phone_number}</p>
+                <p>Date of Birth: {new Date(user?.date_of_birth).toLocaleDateString()}</p>
+            </div>
+            <div className='profile_actions'>
+                <h2>Actions</h2>
+                <Link to='/profile/change-password'>Change password</Link>
+            </div>
+        </div>
+    )
+}
