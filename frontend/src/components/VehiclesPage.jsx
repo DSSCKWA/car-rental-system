6b9154fa196beb5e686d7a82753992db1533597c
Kinga Ramęda <kinga.rameda@interia.pl>
2023-04-18 01:44:50 +0200

    fixed displaying vehicles

--- frontend/src/components/VehiclesPage.jsx
+++ frontend/src/components/VehiclesPage.jsx
@@ -61,12 +61,17 @@ export function VehiclesPage() {
                         <div key={vehicle.vehicle_id} className='vehicle-card'>
                             <img src={`data:image/jpeg;base64,${vehicle.image}`} alt={`${vehicle.brand} ${vehicle.model}`} />
                             <div className='vehicle-details'>
-                                <h3>{vehicle.brand} {vehicle.model}</h3>
-                                <p><b>Year of production: </b> {vehicle.year_of_production}</p>
-                                <p><b>Body type:</b> {vehicle.body_type}</p>
-                                <p><b>Status: </b> {vehicle.status}</p>
-                                <p><b>Engine power:</b> {vehicle.engine_power} KM</p>
-                                <p><b>Fuel type: </b> {vehicle.fuel_type}</p>
+                                <h3 className='vehicle-title'>{vehicle.brand} {vehicle.model}</h3>
+                                <div className='vehicle-details-two-columns'>
+                                    <p><b>Year of production: </b> {vehicle.year_of_production}</p>
+                                    <p><b>Body type:</b> {vehicle.body_type.replace(/_/g, ' ')}</p>
+                                    <p><b>Vehicle class:</b> {vehicle.vehicle_class}</p>
+                                    <p><b>Number of seats:</b> {vehicle.number_of_seats}</p>
+                                    <p><b>Transmission type:</b> manual</p>
+                                    <p><b>Engine power:</b> {vehicle.engine_power} HP</p>
+                                    <p><b>Fuel type: </b> {vehicle.fuel_type}</p>
+                                    <p><b>Drive type:</b> {vehicle.drive_type}</p>
+                                </div>
                             </div>
                         </div>
                         : null,
