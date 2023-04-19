843cbf7d9e60fa8a47ff31f1f42b545a5c20ff68
Karolina <karolinajez21@gmail.com>
2023-04-19 19:36:07 +0200

    Add styles for sidebar layout

--- frontend/src/components/WorkerCarsPage.jsx
+++ frontend/src/components/WorkerCarsPage.jsx
@@ -1,6 +1,6 @@
 export function WorkerCarsPage() {
     return (
-        <div className='worker_cars_page'>
+        <div className='worker_cars_page page_content'>
             <h2>Cars</h2>
         </div>
     )
--- frontend/src/components/WorkerRentalsPage.jsx
+++ frontend/src/components/WorkerRentalsPage.jsx
@@ -1,6 +1,6 @@
 export function WorkerRentalsPage() {
     return (
-        <div className='worker_rentals_page'>
+        <div className='worker_rentals_page page_content'>
             <h2>Rentals</h2>
         </div>
     )
