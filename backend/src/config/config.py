cf150218d1ef28a40d090342f7cb44011a2c9789
Lukas4023 <lukas4023@gmail.com>
2023-05-27 13:32:09 +0200

    Add basic test with login

--- backend/src/config/config.py
+++ backend/src/config/config.py
@@ -7,5 +7,4 @@ class Config:
     SECRET_KEY = "7&Fa2sa23j"
     SQLALCHEMY_DATABASE_URI = 'postgresql://crs_user:CRS@localhost/car_rental_service'
     JSONIFY_PRETTYPRINT_REGULAR = True
-    MAX_CONTENT_LENGTH = 1024*1024*100
-    
+    MAX_CONTENT_LENGTH = 1024 * 1024 * 100
