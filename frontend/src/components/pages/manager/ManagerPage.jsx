8f7eee66c5e5be3db9c080e024ee46a0d6b05468
unknown <dawidgorski0000@gmail.com>
2023-05-30 00:28:20 +0200

    add missing manager link

--- frontend/src/components/pages/manager/ManagerPage.jsx
+++ frontend/src/components/pages/manager/ManagerPage.jsx
@@ -1,6 +1,7 @@
 import { Link, Route, Routes } from 'react-router-dom'
 import { SidebarLayout } from '../../page_elements/SidebarLayout.jsx'
 import { ManagerWorkersPage } from './ManagerWorkersPage.jsx'
+import { ManagerRentalsPage } from './ManagerRentalsPage.jsx'
 import { AddWorkerPage } from './AddWorkerPage.jsx'
 import React from 'react'
 import { useEffect, useState } from 'react'
@@ -18,6 +19,7 @@ export function ManagerPage() {
                 <nav className='manager_menu'>
                     <Link to='/manager/workers' className='manager_menu_item'>Workers</Link>
                     <Link to='/manager/add-worker' className='manager_menu_item'>Add worker</Link>
+                    <Link to='/manager/rentals' className='manager_menu_item'>Rentals</Link>
                     <div className='manager_menu_item report_button' onClick={handleOpen}>Report</div>
                     <ModalComponent open={open} setOpen={setOpen} handleOpen={handleOpen} />
                 </nav>
@@ -27,6 +29,7 @@ export function ManagerPage() {
                     <Routes>
                         <Route path='workers' element={<ManagerWorkersPage />} />
                         <Route path='add-worker' element={<AddWorkerPage />} />
+                        <Route path='rentals' element={<ManagerRentalsPage />} />
                     </Routes>
                 </div>
             </div>
