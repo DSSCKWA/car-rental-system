e28f0e451a89bf755b8d910210e0298ef6d005df
Lukas4023 <lukas4023@gmail.com>
2023-05-29 22:31:24 +0200

    Merge 2

--- backend/src/controllers/vehicles.py
+++ backend/src/controllers/vehicles.py
@@ -1,20 +1,23 @@
-from flask import Blueprint, Response, abort, flash, jsonify, request, session
-from sqlalchemy import and_, not_, or_
+import base64
 from datetime import datetime
-from ..config.extensions import bcrypt, db, login_manager
+
+from flask import Blueprint, abort, jsonify, request, Response
+from flask_login import login_required, current_user
+from sqlalchemy import and_
+
+from ..config.extensions import db
 from ..models.vehicle import Vehicle
-from ..models.price_list import Price_list
 from ..models.rental import Rental
 from ..utils.validator import validate_addition_request_body
+from ..models.price_list import Price_list
 from ..utils.validator import validate_edition_request_body
-import base64
 
 vehicles = Blueprint('vehicles', __name__, url_prefix='/vehicles')
 response_class = Blueprint('response_class', __name__)
 
+
 def get_available_vehicle_ids(start_time, end_time):
-   print(start_time, end_time)
-   if start_time != None and end_time != None:
+    if start_time != None and end_time != None:
         rentals_in_range = (
             Rental.query.with_entities(Rental.vehicle_id)
             .filter(
@@ -26,12 +29,13 @@ def get_available_vehicle_ids(start_time, end_time):
             .subquery()
         )
         return (
-            Vehicle.query.join(Price_list,Vehicle.vehicle_class == Price_list.vehicle_class).filter(~Vehicle.vehicle_id.in_(rentals_in_range))
+            Vehicle.query.join(Price_list, Vehicle.vehicle_class == Price_list.vehicle_class).filter(
+                ~Vehicle.vehicle_id.in_(rentals_in_range))
             .with_entities(Vehicle, Price_list.price).all())
-   else:
-       return  (
-           Vehicle.query.join(Price_list,Vehicle.vehicle_class == Price_list.vehicle_class)
-           .with_entities(Vehicle, Price_list.price).all())
+    else:
+        return (
+            Vehicle.query.join(Price_list, Vehicle.vehicle_class == Price_list.vehicle_class)
+            .with_entities(Vehicle, Price_list.price).all())
 
 
 @vehicles.route('/', methods=['GET'])
@@ -62,44 +66,59 @@ def get_by_id(id):
 
 
 @vehicles.route('/', methods=['POST'])
+@login_required
 def add():
-    new_vehicle_body = request.json
-    validate_addition_request_body(new_vehicle_body)
+    user_permissions = current_user.permissions
+    if user_permissions == "worker":
+        new_vehicle_body = request.json
+        validate_addition_request_body(new_vehicle_body)
 
-    new_vehicle_body["status"] = "available"
-    new_vehicle_body["image"] = base64.b64decode(new_vehicle_body["image"])
-    new_vehicle = Vehicle(new_vehicle_body)
-    db.session.add(new_vehicle)
-    db.session.commit()
-    return new_vehicle.serialize()
+        new_vehicle_body["status"] = "available"
+        new_vehicle_body["image"] = base64.b64decode(new_vehicle_body["image"])
+        new_vehicle = Vehicle(new_vehicle_body)
+        db.session.add(new_vehicle)
+        db.session.commit()
+        return new_vehicle.serialize()
+    else:
+        abort(403, description="Invalid permissions")
 
 
 @vehicles.route('/<int:id>', methods=['PUT'])
+@login_required
 def edit(id):
-    vehicle = Vehicle.query.get(id)
-    if vehicle is None:
-        abort(404, description="Vehicle not found")
-    vehicle_body = request.json
-    vehicle.status = vehicle_body["status"]
-    vehicle.technical_review_date = vehicle_body["technical_review_date"]
-    vehicle.image = base64.b64decode(vehicle_body["image"])
-    vehicle.description = vehicle_body["description"]
-    vehicle.additional_equipment = vehicle_body["additional_equipment"]
-    old_reg=vehicle.registration_number
-    vehicle.registration_number = vehicle_body["registration_number"]
-    vehicle.color = vehicle_body["color"]
+    user_permissions = current_user.permissions
+    if user_permissions == "worker":
+        vehicle = Vehicle.query.get(id)
+        if vehicle is None:
+            abort(404, description="Vehicle not found")
+        vehicle_body = request.json
+        vehicle.status = vehicle_body["status"]
+        vehicle.technical_review_date = vehicle_body["technical_review_date"]
+        vehicle.image = base64.b64decode(vehicle_body["image"])
+        vehicle.description = vehicle_body["description"]
+        vehicle.additional_equipment = vehicle_body["additional_equipment"]
+        old_reg = vehicle.registration_number
+        vehicle.registration_number = vehicle_body["registration_number"]
+        vehicle.color = vehicle_body["color"]
 
-    validate_edition_request_body(vehicle_body,old_reg)
-    db.session.commit()
-    return vehicle.serialize()
+        validate_edition_request_body(vehicle_body, old_reg)
+        db.session.commit()
+        return vehicle.serialize()
+    else:
+        abort(403, description="Invalid permissions")
 
 
 @vehicles.route('/<int:id>', methods=['PATCH'])
+@login_required
 def delete(id):
-    vehicle = Vehicle.query.get(id)
-    if vehicle is None:
-        abort(404, description="Vehicle not found")
-    vehicle_body = request.json
-    vehicle.status = vehicle_body["status"]
-    db.session.commit()
-    return vehicle.serialize()
+    user_permissions = current_user.permissions
+    if user_permissions == "worker":
+        vehicle = Vehicle.query.get(id)
+        if vehicle is None:
+            abort(404, description="Vehicle not found")
+        vehicle_body = request.json
+        vehicle.status = vehicle_body["status"]
+        db.session.commit()
+        return vehicle.serialize()
+    else:
+        abort(403, description="Invalid permissions")
